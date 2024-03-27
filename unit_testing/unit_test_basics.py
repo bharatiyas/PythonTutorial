edc934d921747a2c2efa65adcb5834afa82272e0
bharatiyas <sanjayfromgomia@gmail.com>
2024-03-28 07:15:47 +1000

    Started Unit Testing

---
+++ unit_testing/unit_test_basics.py
@@ -0,0 +1,19 @@
+# PyTest
+# ------
+# 1. It is a testing framework for Python
+# 2. Auto-descovery of tests - pick up tests based on how we name them. 
+#       Ex, based on name of the test file like test_filename.py or function_name_test()
+# 3. Rich assetion introspection - when a test fails we get details on why and where did the test failed
+# 4. Supports parameterized and fixture-based testing - Easily run a test multiple times with different 
+#       arguments. This saves lot of effort.
+# 5. We can run tests individually
+# 6. It has very Simplified Syntax
+# 7. Powerful Fixture System - Enables setup and tear down code
+# 8. Compatibility  - It can support or run with different testing lib
+# 9. Extensibility - Rich plug-in architecture. Like, Django test plugin
+# 10. Easier to use and more powerful than the unittest library shipped defaultly with Python
+
+# Unit test - help you to test your code (and changes made later) quick and easy
+
+
+
